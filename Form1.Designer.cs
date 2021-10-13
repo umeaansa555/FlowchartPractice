514326c2f0170ef431d8b7da4a442810bca54856
P4 <umeaansa555@ed.amdsb.ca>
2021-10-13 13:42:53 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace FlowchartPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(127, 65);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(136, 51);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(92, 152);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(208, 33);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.Text = "Hello World";
+            this.outputLabel.Visible = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(127, 237);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(136, 51);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 352);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FlowchartPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            outputLabel.Visible = true;
+            Thread.Sleep(3000);
+            Refresh();
+
+            this.BackColor = Color.Red;
+            Thread.Sleep(30);
+            Refresh();
+            this.BackColor = Color.Black;
+            Thread.Sleep(30);
+            Refresh();
+            this.BackColor = Color.Red;
+            Thread.Sleep(30);
+            Refresh();
+            this.BackColor = Color.Black;
+            Thread.Sleep(1000);
+            Refresh();
+
+            exitButton.Visible = true;
+            
+            outputLabel.Text = "Good Bye";
+            Thread.Sleep(2500);
+            Refresh();
+            Application.Exit();
+
+        }
+    }
+}
